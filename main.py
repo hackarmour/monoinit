3daff39ca034a3704b709417418ac33277b23513
Emperor <83999665+TheEmperor342@users.noreply.github.com>
2022-02-11 17:41:39 +0530

    fix: commit formatting; feat: Add help command

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 # === IMPORT MODULES === #
-import os, typing
-
-exit_ = False
+import os, typing, argparse
 
 # === GET TODOS === #
 def todos(folder: str) -> str:
@@ -38,40 +36,122 @@ def todos(folder: str) -> str:
     fancyReturn = ""
 
     n = "\n\t"
+    print(list(files.keys()))
     for i in list(files.keys()):
-        fancyReturn += f"Path: {i[len(c)+1:]}\n\t{n.join(files[i])}"
+        fancyReturn += f"Path: {i[len(c)+1:]}\n\t{n.join(files[i])}\n"
+
     return fancyReturn
 
 # === SHELL === #
 def shell(command: str) -> typing.Any:
+    global PARENT_DIR
 
     # === CHANGE DIRECTORY === #
-    if command.startswith("cd"):
+    if command.lower().startswith("cd"):
         if len((x := command.strip().split())) == 1: return "The path has not been supplied"
         elif len(x) > 2: return "Extra arguments passed"
         elif not os.path.isdir(x[1]): return "Cannot find the path specified"
+
+        # === TO CHECK IF THE PATH SPECIFIED GOES BEYOND PARENT_DIR === #
+        if PARENT_DIR not in os.path.abspath(x[1]):
+            print("Path specified goes beyond the parent directory")
+            return
+
         os.chdir(x[1])
     
     # === GET TODOS === #
-    elif command.startswith("todos"):
+    elif command.lower().startswith("todos"):
         if len(command.strip().split()) > 1: return "Extra arguments passed"
         return todos(os.getcwd())
 
     # === COMMIT MESSAGE === #
-    elif command.startswith("git commit -m"):
-        b = "\""
-        commit = f'{os.path.basename(os.getcwd())}: {command.split(f"{b}")[1]}'
-        os.system(f'git commit -m \"{commit}\"')
+    elif command.lower().startswith("git"):
         
+        # === KEEP THE CURRENT PATH === #
+        cur_path = os.getcwd()
+
+        # === CHANGE PATH TO PARENT DIRECTORY === #
+        os.chdir(PARENT_DIR)
+
+        # === IF THE COMMAND IS A COMMIT === #
+        if command.lower().startswith("git commit -m"):
+            # === COMMIT FORMATTING === #
+            b = "\""
+            commit = command.split(f"{b}")
+            commit[1] = f'{os.path.basename(os.getcwd())}: {command.split(f"{b}")[1]}'
+
+            # === RUN COMMAND === #
+            os.system("\"".join(commit))
+            
+            # === EXIT FUNCTION === #
+            return
+        
+        # === RUN THE GIT COMMAND SPECIFIED BY USER === #
+        os.system(command)
+        os.chdir(cur_path)
+        
+    # === HELP === #
+    elif command.lower().startswith("help"):
+        return """
+Command: cd
+    Usage: cd <path>
+    Used to change the current working directory
+
+Command: todos
+    Usage: todos
+    Used to get all the ToDos from monorepos/monorepo
+
+Command: exit
+    Usage: exit
+    To exit the shell
+
+You can use this shell as if you are using your terminal.
+Any other command is executed by your default shell
+"""
+
     # === TO EXIT === #
-    elif command.startswith("exit"):
+    elif command.lower().startswith("exit"):
         global exit_
         exit_ = True
 
     else: os.system(command)
 
 if __name__ == "__main__":
+
+    # === SETUP ARGPARSE === #
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'path',
+        metavar='path/to/monorepo',
+        type=str,
+        nargs='+',
+        help='Input monorepo path to monoinit'
+    )
+
+    # === GET ARGUMENTS === #
+    args = parser.parse_args()
+
+    # === IF THE PATH DOESN'T EXIST === #
+    if not os.path.isdir(args.path[0]):
+        print("Path not recognized")
+        exit()
+
+    # === TO CHECK IF THE DIRECTORY CONTAINS MONOREPOS === #
+    elif "workflow.json" not in os.listdir(args.path[0]):
+        print("The path specified doesn't have a workflow.json file")
+        exit()
+
+    # === CHANGE DIRECTORY TO THE PATH === #
+    os.chdir(args.path[0])
+
+    # === SAVE THE PATH TO PARENT DIR === #
+    PARENT_DIR = os.getcwd()
+    
+    # === IF THIS TURNS TRUE, THE SCRIPT STOPS === #
+    exit_ = False
+
     while not exit_:
+        # === GET AND PRINT OUTPUT === #
         output = shell(input(f"◆ {os.getcwd()} ❯❯❯ "))
         if output is None: continue
         else: print(output)
